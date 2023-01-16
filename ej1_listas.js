9bce9a67b74007a9f8963f71275f38886ba6306c
germanludena <115158174+germanludena@users.noreply.github.com>
2023-01-16 14:42:56 +0100

    Update ej1_listas.js

--- ej1_listas.js
+++ ej1_listas.js
@@ -1,13 +1,37 @@
     // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+var lista_pares=[]
+var i= 0
+while(i<=20){
+    lista_pares.push(i);
+    i=i+1;
+    
+}
+console.log("ListaDeNumeros: "+lista_pares);
 
-    // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+   // Crea una lista de números impares del 1 al 19 utilizando un bucle for
     
+   var lista_impares=[]
+   for(var index =1; index<=19; index++){
+    if(index % 2==1){
+        lista_impares.push(index);
+    }
+   }
+    console.log("Impares:" +lista_impares);
+
     // Crea una lista de comida.
+
+    var lista_comida =["tortilla","paella","durum"]
+    console.log(lista_comida);
+
+
+     // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
     
-    // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
-    
+     lista_comida.unshift("macarrones");
+     console.log("ComidasListadas:"+ lista_comida);
     // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
     
+     lista_comida[1] = "sopa de arroz";
+     console.log(lista_comida);
     // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
     
     // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
